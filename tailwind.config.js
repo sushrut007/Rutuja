b95e30c231daba370bce8f8e1fe12fdccd135ad8
Sushrut007 <sushrutmhetras@hmail.com>
2025-12-14 20:49:01 +0530

    [#9] Fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FE4300",
+        secondary: "#868686",
+        softGray: "#F0F0F0",
+        mistGray: "#D7DCDD",
+        gray: "#C0D8E0"
+      },
+      screens: {
+        xs: "425px"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
